6feec5963c8758e738018c06cba2ea6ea43a22ca
Suraj <surajbhansingh588@gmail.com>
2021-05-30 02:04:08 +0530

    UI for character info

--- src/pages/Person/index.js
+++ src/pages/Person/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Character from '../../components/Character/Character';
 import './index.css';
 
 function Person() {
   return (
     <div className="person">
-      <h1>Luke Skywalker</h1>
+      <Character/>
     </div>
   );
 }
